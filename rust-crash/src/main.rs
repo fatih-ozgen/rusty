71fc2b68f14e4b7667d714991fe66ecbdba504e4
fatihozgen <fatihozgen.85@gmail.com>
2023-02-22 01:58:15 +0200

    variables

--- rust-crash/src/main.rs
+++ rust-crash/src/main.rs
@@ -1,5 +1,15 @@
 #![deny(clippy::all)]
 
 fn main() {
-    println!("Karrrdeşim sokuyorsun!");
+    let _first_name = "Fatih";
+    let _last_name = "Özgen";
+    let _age = 37;
+    let _job = "engineer";
+    let _percentage = 0.000_001;
+    println!(
+        "My name is {} {} and my job is {}",
+        _first_name, _last_name, _job
+    );
+
+    println!("Aşkta şansım %{}", _percentage)
 }
