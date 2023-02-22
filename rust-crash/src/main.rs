9df4f22f30d62d7f2d4a457e95349a14db7e1fd4
fatihozgen <fatihozgen.85@gmail.com>
2023-02-22 21:14:00 +0200

    deleted

--- rust-crash/src/main.rs
+++ rust-crash/src/main.rs
@@ -1,15 +1,6 @@
 #![deny(clippy::all)]
 
 fn main() {
-    let _first_name = "Fatih";
-    let _last_name = "Özgen";
-    let _age = 37;
-    let _job = "engineer";
-    let _percentage = 0.000_001;
-    println!(
-        "My name is {} {} and my job is {}",
-        _first_name, _last_name, _job
-    );
-
-    println!("Aşkta şansım %{}", _percentage)
+    let _name = "Foo";
+    let _name = "Bar";
 }
